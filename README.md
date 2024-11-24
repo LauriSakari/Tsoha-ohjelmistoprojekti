ac350fe1df3a861162ec06f18759e6d552b97c18
LauriSakari <lauri.harjukoski@gmail.com>
2024-11-24 22:42:13 +0200

    session cleared when logged out, better error handlin in add_user

--- README.md
+++ README.md
@@ -25,16 +25,21 @@ Tilanne 17.11.24:
 Käynnistysohjeet:
 
 Kloonaa tämä repositorio omalle koneellesi ja siirry sen juurikansioon. Luo kansioon .env-tiedosto ja määritä sen sisältö seuraavanlaiseksi:
+```
     DATABASE_URL=<tietokannan-paikallinen-osoite>
     SECRET_KEY=<salainen-avain>
-
+```
 Seuraavaksi aktivoi virtuaaliympäristö ja asenna sovelluksen riippuvuudet komennoilla:
+```
     $ python3 -m venv venv
     $ source venv/bin/activate
     $ pip install -r ./requirements.txt
-
+```
 Määritä vielä tietokannan skeema komennolla:
+```
     $ psql < schema.sql
-
+```
 Nyt voit käynnistää sovelluksen komennolla:
+```
     $ flask run
+```
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, request, session
 from os import getenv
+from sqlalchemy.exc import IntegrityError
 import user_handling
 import messages
 import time_slots
@@ -9,7 +10,8 @@ app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    list = messages.get_list()
+    return render_template("index.html", count=len(list), messages=list)
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -20,7 +22,6 @@ def login():
     user = user_handling.find_username(username)
 
     if not user:
-        print("invalid username")
         return render_template("error.html", message="Invalid username")
     else:
         hash_value = user.password
@@ -30,7 +31,7 @@ def login():
         else:
             return render_template("error.html", message="Invalid password")
 
-    return redirect("/home")
+    return redirect("/")
 
 @app.route("/register")
 def register():
@@ -38,7 +39,9 @@ def register():
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
-    print("request form", request.form)
+    if len(request.form) < 5:
+        return render_template("error.html", message="Täytä kaikki kentät")
+
     username = request.form["username"]
     password = request.form["password"]
     grade = request.form["grade"]
@@ -51,9 +54,8 @@ def add_user():
 
     try:
         user_id = user_handling.add_user(username, hash_value)
-    except Exception as e:
-        return render_template("error.html", message = e)
-
+    except IntegrityError:
+        return render_template("error.html", message = "Käyttäjänimi on jo käytössä")
     try:
         user_handling.add_userinfo(user_id, grade, style)
     except Exception as e:
@@ -62,19 +64,14 @@ def add_user():
     session ["username"] = username
     session ["user_id"] = user_id
 
-    return redirect("/home")
-
-@app.route("/home")
-def home():
-    list = messages.get_list()
-    return render_template("home.html", count=len(list), messages=list)
+    return redirect("/")
 
 @app.route("/send_message", methods=["POST"])
 def send():
     content = request.form["content"]
     user_id = session["user_id"]
     if messages.send(content, user_id):
-        return redirect("/home")
+        return redirect("/")
     else:
         return render_template("error.html", message="Viestin lähetys ei onnistunut")
 
@@ -92,11 +89,12 @@ def add_slot():
         result = time_slots.send(session["user_id"], date, starting_time, finishing_time)
 
         print(result)
-        return redirect("/home")
+        return redirect("/")
     if request.method == "GET":
         return render_template("add_slot.html")
 
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
